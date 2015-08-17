e68e8b6dc4c88eaa983a6a922d3ac7b79aaf1c78
Henri Sweers <pandanomic@gmail.com>
2015-08-17 02:48:46 -0700

    Improve checkForWinner logic
    This should slightly improve the performance of checking for the winner in the following ways:
    - Only check rows/columns/diagonals that the newly-set index lies within
    - Break out as soon as we hit something that isn't the current player
    - Only need to check for a winner once at the beginning of minimax, as we can store the result and check it in both cases

--- app/src/main/java/io/sweers/tictactoe/TicTacToeGame.java
+++ app/src/main/java/io/sweers/tictactoe/TicTacToeGame.java
@@ -34,13 +34,39 @@ public final class TicTacToeGame {
     private static final int GRID_SIZE = 9;
 
     private char[] grid = {NONE, NONE, NONE, NONE, NONE, NONE, NONE, NONE, NONE};
-    private int[] winningIndices = null;
+    private int[] winningIndices = {-1, -1, -1};
     private char currentPlayer;
     private boolean isOver = false;
     private int nextCpuMove;
     @GameState private int gameState = CONTINUE;
     private OnGameOverListener onGameOverListener;
     private final Random random = new Random();
+
+    // Mapping of grid indices to their x,y coordinates
+    private static final int[][] COORDINATES_MAPPING = {
+            new int[] {0, 0},
+            new int[] {1, 0},
+            new int[] {2, 0},
+            new int[] {0, 1},
+            new int[] {1, 1},
+            new int[] {2, 1},
+            new int[] {0, 2},
+            new int[] {1, 2},
+            new int[] {2, 2}
+    };
+
+    // Mapping of coordinates to indices
+    private static final int[][] INDEX_MAPPING = {
+            new int[] {0, 1, 2},
+            new int[] {3, 4, 5},
+            new int[] {6, 7, 8}
+    };
+
+    private static final boolean[] DIAGONALS = {
+            true, false, true,
+            false, true, false,
+            true, false, true
+    };
 
     public TicTacToeGame() {
         boolean playerOneGoesFirst = random.nextBoolean();
@@ -56,12 +82,11 @@ public final class TicTacToeGame {
     }
 
     public void makeMove(int position) {
-        char prevPlayer = currentPlayer;
         grid[position] = currentPlayer;
 
         currentPlayer = currentPlayer == PLAYER_ONE ? PLAYER_TWO : PLAYER_ONE;
 
-        if (checkForWinner(prevPlayer) != CONTINUE) {
+        if (checkForWinner(position) != CONTINUE) {
             endGame();
         }
     }
@@ -71,49 +96,76 @@ public final class TicTacToeGame {
     }
 
     @GameState
-    private int checkForWinner(char player) {
-        boolean matched = false;
-
-        // Check rows
-        for (int i = 0; i <= 6; i += 3) {
-            winningIndices = new int[]{i, i + 1, i + 2};
-            matched = checkMatches(winningIndices, player);
-            if (matched) {
-                break;
-            }
-        }
+    private int checkForWinner(int newIndex) {
+        boolean winnerFound = true;
+        int[] coordinates = COORDINATES_MAPPING[newIndex];
+        char player = grid[newIndex];
+        int x = coordinates[0];
+        int y = coordinates[1];
 
         // Check columns
-        if (!matched) {
-            for (int i = 0; i <= 2; i++) {
-                winningIndices = new int[]{i, i + 3, i + 6};
-                matched = checkMatches(winningIndices, player);
-                if (matched) {
+        for (int i = 0; i < 3; ++i) {
+            int index = INDEX_MAPPING[i][x];
+            if (grid[index] != player) {
+                winnerFound = false;
+                break;
+            }
+            winningIndices[i] = index;
+        }
+
+        // Check rows
+        if (!winnerFound) {
+            winnerFound = true;
+            for (int i = 0; i < 3; ++i) {
+                int index = INDEX_MAPPING[y][i];
+                if (grid[index] != player) {
+                    winnerFound = false;
                     break;
                 }
+                winningIndices[i] = index;
             }
         }
 
         // Check diagonals
-        if (!matched) {
-            winningIndices = new int[]{0, 4, 8};
-            matched = checkMatches(winningIndices, player);
-        }
-        if (!matched) {
-            winningIndices = new int[]{2, 4, 6};
-            matched = checkMatches(winningIndices, player);
+        if (!winnerFound && DIAGONALS[newIndex]) {
+            winnerFound = true;
+            boolean checkBoth = newIndex == 4;
+            if (checkBoth || newIndex == 0 || newIndex == 8) {
+                // Left to right
+                for (int i = 0; i < 3; ++i) {
+                    int index = INDEX_MAPPING[i][i];
+                    if (grid[index] != player) {
+                        winnerFound = false;
+                        break;
+                    }
+                    winningIndices[i] = index;
+                }
+            } else {
+                winnerFound = false;
+            }
+            if (!winnerFound && (checkBoth || newIndex == 2 || newIndex == 6)) {
+                winnerFound = true;
+                // Right to left
+                for (int i = 2; i < 7; i += 2) {
+                    if (grid[i] != player) {
+                        winnerFound = false;
+                        break;
+                    }
+                    winningIndices[(i / 2) - 1] = i;
+                }
+            }
         }
 
 
         @GameState int result = TIE;
-        if (!matched) {
+        if (!winnerFound) {
             // No winner yet, continue if there are any open spaces left
-            winningIndices = null;
-            for (int i = 0; i < GRID_SIZE; ++i) {
-                if (grid[i] == NONE) {
-                    result = CONTINUE;
-                    break;
-                }
+            // Clear winning indices
+            winningIndices[0] = -1;
+            winningIndices[1] = -1;
+            winningIndices[2] = -1;
+            if (!getAvailableStates().isEmpty()) {
+                result = CONTINUE;
             }
         } else {
             result = player == PLAYER_ONE ? ONE_WINS : TWO_WINS;
@@ -126,17 +178,8 @@ public final class TicTacToeGame {
     public void endGame() {
         isOver = true;
         if (onGameOverListener != null) {
-            onGameOverListener.onGameOver(gameState, winningIndices);
+            onGameOverListener.onGameOver(gameState, winningIndices[0] == -1 ? null : winningIndices);
         }
-    }
-
-    private boolean checkMatches(int[] indices, char player) {
-        boolean matches = true;
-        for (int index : indices) {
-            matches = matches && grid[index] == player;
-        }
-
-        return matches;
     }
 
     public Observable<Object> getCpuMove() {
@@ -148,7 +191,7 @@ public final class TicTacToeGame {
                             // minimax will spend a lot of time calculating every permutation of this, but always ends on 0. Let's spice it up
                             nextCpuMove = new Random().nextInt(grid.length);
                         } else {
-                            minimax(0, PLAYER_TWO);
+                            minimax(0, PLAYER_TWO, -1);
                         }
                     }
                 });
@@ -244,17 +287,17 @@ public final class TicTacToeGame {
      * @return the maximized score if this is the computer (PLAYER_TWO)
      *         or minimized score if human (PLAYER_ONE)
      */
-    private int minimax(int depth, char player) {
-
-        if (checkForWinner(PLAYER_TWO) == TWO_WINS) {
-            return 10 - depth;
-        } else if (checkForWinner(PLAYER_ONE) == ONE_WINS) {
-            return depth - 10;
-        }
-
+    private int minimax(int depth, char player, int newIndex) {
         List<Integer> pointsAvailable = getAvailableStates();
-        if (pointsAvailable.isEmpty()) {
-            return 0;   // Ties help no one
+        if (depth != 0) {
+            @GameState int currentResult = checkForWinner(newIndex);
+            if (currentResult == TWO_WINS) {
+                return 10 - depth;
+            } else if (currentResult == ONE_WINS) {
+                return depth - 10;
+            } else if (pointsAvailable.isEmpty()) {
+                return 0;   // Ties help no one
+            }
         }
 
         ++depth;
@@ -267,7 +310,7 @@ public final class TicTacToeGame {
             int index = pointsAvailable.get(i);
             if (player == PLAYER_TWO) {
                 makeDummyMove(index, PLAYER_TWO);
-                int score = minimax(depth, PLAYER_ONE);
+                int score = minimax(depth, PLAYER_ONE, index);
                 if (score > max
                         || (score == max && random.nextBoolean())) {    // Equally good options, so randomly choose one for added flavor
                     max = score;
@@ -275,7 +318,7 @@ public final class TicTacToeGame {
                 }
             } else if (player == PLAYER_ONE) {
                 makeDummyMove(index, PLAYER_ONE);
-                int score = minimax(depth, PLAYER_TWO);
+                int score = minimax(depth, PLAYER_TWO, index);
                 if (score < min
                         || (score == min && random.nextBoolean())) {    // Equally good options, so randomly choose one for added flavor
                     min = score;
